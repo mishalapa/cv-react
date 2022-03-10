ba5e48a3259a77e08af28ecc071a690df62827f8
mishalapa <mishamedme@gmail.com>
2022-03-10 18:07:09 +0300

    feat: add new cv

---
+++ src/components/Header.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import avatar from  "../image/avatar1.jpg"
+import git from '../image/github.svg'
+import letter from '../image/letter.png'
+import phone from '../image/phone.png'
+import map from  '../image/map.png'
+import telegram from "../image/telegram.svg"
+import discord from '../image/discord.svg'
+import boardgame from '../image/boardgame.png'
+import hiking from '../image/hiking.png'
+import detector from '../image/detector.png'
+
+
+
+const Header = () => {
+    return (
+        <header className="header">
+        <div className='avatar'>
+          <img className='avatar__image' src={avatar}/>
+          <div className='avatar__title'>
+            <p>Mikhail</p>
+            <p>Lapushynski</p>
+            <h4>Front-end developer</h4>
+          </div>
+        </div>
+        <div className="skills__interest">
+            <div className='skills'>
+                <h2>SKILLS</h2>
+                <ul className="skills__wrapper">
+                <li className="skills__item">
+                    <p>JavaScript</p>
+                </li>
+                <li className="skills__item">
+                    <p>React</p>
+                </li>
+                <li className="skills__item">
+                    <p>HTML/CSS/SCSS</p>
+                </li>
+                <li className="skills__item">
+                    <p>Git</p>
+                </li>
+                <li className="skills__item">
+                    <p>Webpack</p>
+                </li>
+                <li className="skills__item">
+                    <p>Figma</p>
+                </li>
+            </ul>
+            </div>
+            <div className="interest">
+                <h2>INTERESTS</h2>
+                <div className="interest__wrapper">
+                    <div className="interest__item">
+                        <img src={boardgame}/>
+                        <p>Board Games</p>
+                    </div>
+                    <div className="interest__item">
+                        <img src={hiking}/>
+                        <p>Hiking</p>
+                    </div>
+                    <div className="interest__item">
+                        <img src={detector}/>
+                        <p>Metal Detector</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="contact">
+            <h2>CONTACTS</h2>
+          <ul className="contact__wrapper">
+            <li className="contact__item">
+                <img src={letter}/>
+                <a target="_blank" href="https://mail.google.com/mail/u/0/#inbox">mishamedme@gmail.com</a>
+            </li>
+            <li className="contact__item">
+                <img src={telegram}/>
+                <a target="_blank" href="https://telegram.me/Misha_lapa">@Misha_lapa</a>
+            </li>
+            <li className="contact__item">
+                <img src={phone}/>
+                <a target="_blank" href="tel:+375339133961">+375339133961</a>
+            </li>
+            <li className="contact__item">
+                <img src={discord}/>
+                <a>mishalapa#6203</a>
+            </li>
+            <li className="contact__item">
+                <img src={map}/>
+                <a target="_blank" href="https://www.google.com/maps/place/%D0%9C%D0%B8%D0%BD%D1%81%D0%BA/@53.6704045,26.7069003,8.14z/data=!4m5!3m4!1s0x46dbcfd35b1e6ad3:0xb61b853ddb570d9!8m2!3d53.9006011!4d27.558972?hl=ru">Minsk</a>
+            </li>
+            <li className="contact__item">
+                <img src={git}/>
+                <a target="_blank" href="https://github.com/mishalapa">github.com/mishalapa</a>
+            </li>
+          </ul>
+        </div>
+    </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <main className="main">
+            <div className="other">
+                <div className="languages">
+                    <h2>LANGUAGES</h2>
+                    <div className="languages__wrapper">
+                        <div className="languages__item">
+                            <h3>ENGLISH</h3>
+                            <p>A2+</p>
+                        </div>
+                        <div className="languages__item">
+                            <h3>GERMAN</h3>
+                            <p>A2</p>
+                        </div>
+                        <div className="languages__item">
+                            <h3>BELARUSSIAN</h3>
+                            <p>Native</p>
+                        </div>
+                        <div className="languages__item">
+                            <h3>RUSSIAN</h3>
+                            <p>Native</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="education">
+                    <h2>EDUCATION</h2>
+                    <div>
+                        <h3>Belarusian National Technical University.</h3>
+                        <h3>Energy Engineer.</h3>
+                        <p>2013-2018.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="about__me">
+                <h2>ABOUT ME</h2>
+                <p>Let me introduce myself. I am twenty six years old. I was born in Postawy, but now live in Minsk.</p>
+                <p>I do not have commercial experience, but I hope that I can compensate for this with my desire  for work. Now I am developing my skills in working with <span>ECMAScript 6, React.</span></p>
+                <p>I also study <span>English</span>. I did not know English before. I studied German at School. I am going to improve my skills not only in English, but also in an IT sphere.</p>
+            </div>
+            <div className="projects">
+                <h2>MY PROJECTS</h2>
+                <ul className="projects__wrapper">
+                    <li className="projects__item">
+                        <a target="_blank" href="https://rolling-scopes-school.github.io/mishalapa-JSFEPRESCHOOL/memory-game/">Memory Game</a>
+                        <p>Description: A game for the development of memory. Everyone must make their choice! Marvel or DC!</p>
+                        <p>Stack: DOM Events, CSS Animations, Adaptive, Algorithms, localStorage.</p>
+                    </li>
+                    <li className="projects__item">
+                        <a target="_blank" href="https://mishalapa.github.io/Calculator/">Calculator</a>
+                        <p>Description: You can do your calculations here. You can also use the keyboard.</p>
+                        <p>Stack: DOM Events, Adaptive.</p>
+                    </li>
+                    <li className="projects__item">
+                        <a target="_blank" href="https://rolling-scopes-school.github.io/mishalapa-JSFEPRESCHOOL/image-galery/">Image Galery</a>
+                        <p>Description: Enter a request and enjoy the photos.</p>
+                        <p>Stack:  DOM Events, CSS Animations, Adaptive markup, Web Photo API.</p>
+                    </li>
+                    <li className="projects__item">
+                        <a target="_blank" href="https://rolling-scopes-school.github.io/mishalapa-JSFEPRESCHOOL/portfolio/">Landing Page</a>
+                        <p>Description: An example of a landing page made by me from the rs-school course</p>
+                        <p>Stack: DOM Events, CSS Animations, Adaptive markup,  localStorage, pixelPerfect markup.</p>
+                    </li>
+                    <li className="projects__item">
+                        <a target="_blank" href="https://rolling-scopes-school.github.io/mishalapa-JSFEPRESCHOOL/custom-video/">Video Player</a>
+                        <p>Description: Custom video player with videos about my pets</p>
+                        <p>Stack: DOM Events, CSS Animations.</p>
+                    </li>
+                </ul>
+            </div>
+        </main>
+    )
+}
+
+export default Main;
